78f4fa23b430ccbc896062274d36ece5b92ee684
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-04-21 15:11:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,16 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include <limits.h>
+
 /**
- * struct form - Types.
- * @c: Types in c.
+ * struct print - struct for printer functions.
+ * @type_arg: identifier
  * @f: Corresponding function.
- **/
-typedef struct form
+ *
+ * Description: struct that stores pointers to a printer function
+ */
+typedef struct print
 {
-  char c;
-  int (*f)(va_list);
+   char *type_arg;
+   int (*f)(va_list);
 } type_printer;
+
 int _printf(const char *format, ...);
 int print_s(va_list arg);
 int print_c(va_list arg);
@@ -25,4 +35,5 @@ int print_u(va_list arg);
 int print_x(va_list arg);
 int print_X(va_list arg);
 int print_o(va_list arg);
+
 #endif
