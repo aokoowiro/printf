3511e23c5dd19375516990773554a59a584f0cc7
Okemwag <gabrielokemwa83@gmail.com>
2022-04-22 08:22:34 -0700

    print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+/**
+* op_percent - main function
+* @p: The argument pointer.
+*
+* Description: This function prints a percent.
+*
+* Return: 1.
+*/
+int op_percent(va_list __attribute__((unused)) p)
+{
+	_putchar('%');
+	return (1);
+}
